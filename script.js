21b6a56d63e05b29d8b0412f408e442b6308d0b6
Ryan <ryan.wy.wong88@gmail.com>
2018-11-02 17:00:58 -0400

    added leaderboard functionality

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const difficulty = Array.from(document.querySelectorAll("[type=radio]"));
 //using Array.from to convert node list to array
 const targets = Array.from(document.querySelectorAll(".targets")); 
 const span = document.querySelector("span");
-//setting a boolean which starts as false, but after a certain amount of time changes to true and the game stops
-let gameOver = false;
+const grabLeaderboard = document.querySelector(".leaderboard ul");
 let score = 0;
+let finalScore = 0;
 let isClicked = false;
+let countTime = 0;
+const leaderboardArray = JSON.parse(localStorage.getItem("items")) || [];
 
 //random index generator, depending on the size of the targets array
 function randomIndex (array) {
@@ -55,27 +57,39 @@ function randomTarget (level) {
 function handleSubmit(e) {
     //this stops the form from refreshing
     e.preventDefault(); 
-    const value = userName.value;
+    // clearInterval(gameOn);
+    const leaderboard = {};
+    const name = userName.value;
+
+    //resets gameOver to false boolean everytime start button is hit
+    gameOver = false;
+    countTime = 0;
 
     //looks through the radio buttons for the checked one and returns the value. If none are checked, default to medium
     const level = difficulty.filter(value => value.checked)
         .map(value => value.value)
         .toString() || "medium";
     
+    //right after start button, run the function once, then let interval kick in
     randomTarget(level);
-    //loops through the randomTarget function until gameOver boolean becomes true
+
+    //loops through the randomTarget function until countTime has incremented to 5
     const gameOn = setInterval(() => {
-        if(gameOver) {
+        if(countTime === 5) {
             clearInterval(gameOn);
+            setTimeout(() => {
+                leaderboard.name = name;
+                leaderboard.finalScore = score;
+                leaderboardArray.push(leaderboard);
+                localStorage.setItem("items", JSON.stringify(leaderboardArray));
+                displayLeaderboard(leaderboardArray);
+                score = 0;
+            }, 2000);
         }
         isClicked = false;
+        countTime++;
         randomTarget(level);
     },2000);
-
-    //this sets the duration of how long the game goes, per click of the start button
-    setTimeout(() => {
-        gameOver = true;
-    }, 10000)
 
     //this removes the text in the input after submit
     this.reset(); 
@@ -88,5 +102,24 @@ function handleScore (e) {
     span.textContent = score;
 }
 
+function displayLeaderboard (array) {
+    const display = array
+        .sort((a,b) => {
+            if(a.finalScore > b.finalScore) {
+                return -1;
+            }
+            else {
+                return 1;
+            }
+        })
+        .map(leader => {
+            return `
+                <li>${leader.name} ${leader.finalScore}</li>
+            `;
+        }).join("");
+    grabLeaderboard.innerHTML = display;
+}
+
+displayLeaderboard(leaderboardArray);
 form.addEventListener("submit", handleSubmit);
 targets.forEach(target => target.addEventListener("click", handleScore));
